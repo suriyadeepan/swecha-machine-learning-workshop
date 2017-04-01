02f263f9a3a2615158f0dc18d2bbfa0f7635b768
Suriyadeepan Ram <suriyadeepan.r@gmail.com>
2017-04-01 23:15:16 +0530

    add tutorial on a classification problem

--- notebooks/helper.py
+++ notebooks/helper.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def show_mnist_image(plt, im, cmap=None):
     assert im.shape[0] == 784
     # reshape
@@ -11,3 +14,28 @@ def show_mnist_image_multi(plt, ims, cmap=None):
     for im in ims_reshaped:
         plt.figure()
         plt.imshow(im, cmap=cmap)
+
+
+def read_wine_data(filename='data/wine.txt'):
+
+    def process(split_line):
+        x, y = [], []
+        for i, item in enumerate(split_line):
+            if i > 12:
+                y.append(int(item))
+            else:
+                x.append(float(item))
+        return x,y
+
+    def read_file():
+        with open(filename, 'r') as f:
+            return [ process(line.split('\t')) for line in f.read().split('\n')[:-1] ]
+
+    content = read_file()
+
+    X, Y = [], []
+    for x, y in content:
+        X.append(x)
+        Y.append(y)
+
+    return np.array(X, dtype=np.float32), np.array(Y, dtype=np.int64)
